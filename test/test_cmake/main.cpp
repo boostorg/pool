80a46b8c9e6e5440810cd4dd93662461f9d6a28f
Mike Dev <mike.dev@gmx.de>
2019-01-05 07:07:18 +0100

    [CMake] Add test for cmake file

---
+++ test/test_cmake/main.cpp
@@ -0,0 +1,19 @@
+/* Copyright 2018 Mike Dev
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at https ://www.boost.org/LICENSE_1_0.txt
+ */
+
+/* Verfiy that we can find all hederfiles
+ * from this library as well as the
+ * transitively included ones
+ */
+#include <boost/pool/poolfwd.hpp>
+#include <boost/pool/object_pool.hpp>
+#include <boost/pool/pool.hpp>
+#include <boost/pool/pool_alloc.hpp>
+#include <boost/pool/simple_segregated_storage.hpp>
+#include <boost/pool/singleton_pool.hpp>
+
+int main() {
+
+}
