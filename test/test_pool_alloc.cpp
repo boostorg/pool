boostorg/pool

commit 8ec1be1e82ba559744ecfa3c6ec13f71f9c175cc

Fix the Broken `malloc_n` Behavior (#53)
* Fix the Broken `malloc_n` Behavior

* Fix the Broken `malloc_n` Behavior

* Fix the Broken `malloc_n` Behavior

Added Unit Tests

* Fix the Broken `malloc_n` Behavior

Add more unit tests.

* Fix the Broken `malloc_n` Behavior

* Fix the Broken `malloc_n` Behavior

* Fix the Broken `malloc_n` Behavior

test/test_pool_alloc.cpp
@@ -263,6 +263,34 @@ void test_mem_usage()
         //  This will clean up the memory leak from (*B*)
     }
 
+    {
+        pool_type pool(sizeof(int), 2);
+        void * ptr_0 = pool.malloc();
+        void * ptr_1 = pool.malloc();
+        void * ptr_2 = pool.malloc();
+        void * ptr_3 = pool.malloc();
+        pool.ordered_free(ptr_2);
+        pool.ordered_free(ptr_3);
+        BOOST_TEST(pool.release_memory());
+        pool.ordered_free(ptr_0);
+        pool.ordered_free(ptr_1);
+        BOOST_TEST(pool.release_memory());
+    }
+
+    {
+        pool_type pool(sizeof(int), 2);
+        void * ptr_0 = pool.malloc();
+        void * ptr_1 = pool.malloc();
+        void * ptr_2 = pool.malloc();
+        void * ptr_3 = pool.malloc();
+        pool.ordered_free(ptr_0);
+        pool.ordered_free(ptr_1);
+        BOOST_TEST(pool.release_memory());
+        pool.ordered_free(ptr_2);
+        pool.ordered_free(ptr_3);
+        BOOST_TEST(pool.release_memory());
+    }
+
     BOOST_TEST(track_alloc::ok());
 }
 

include/boost/pool/simple_segregated_storage.hpp
@@ -328,6 +328,19 @@ void * simple_segregated_storage<SizeType>::try_malloc_n(
     void * & start, size_type n, const size_type partition_size)
 {
   void * iter = nextof(start);
+  if (n == 1)
+  {
+    void * next = nextof(iter);
+    if (next != static_cast<char *>(iter) + partition_size)
+    {
+      start = iter;
+      return 0;
+    }
+    else
+    {
+      return iter;
+    }
+  }
   while (--n != 0)
   {
     void * next = nextof(iter);

test/test_simple_seg_storage.cpp
@@ -278,6 +278,82 @@ int main()
         }
 
         {
+            char* const pc = track_allocator::malloc(4 * partition_sz);
+            test_simp_seg_store tstore;
+            tstore.add_ordered_block(pc, 4 * partition_sz, partition_sz);
+
+            void* pvret = tstore.malloc_n(1, partition_sz);
+            BOOST_TEST(pvret == pc);
+
+            // There should still be two contiguous
+            //  and one non-contiguous chunk left
+            std::size_t nchunks = 0;
+            while(!tstore.empty())
+            {
+                tstore.malloc();
+                ++nchunks;
+            }
+            BOOST_TEST(nchunks == 3);
+        }
+
+        {
+            char* const pc = track_allocator::malloc(4 * partition_sz);
+            test_simp_seg_store tstore;
+            tstore.add_ordered_block(pc, 4 * partition_sz, partition_sz);
+
+            void* pvret = tstore.malloc_n(2, partition_sz);
+            BOOST_TEST(pvret == pc);
+
+            // There should still be two contiguous
+            //  and one non-contiguous chunk left
+            std::size_t nchunks = 0;
+            while(!tstore.empty())
+            {
+                tstore.malloc();
+                ++nchunks;
+            }
+            BOOST_TEST(nchunks == 2);
+        }
+
+        {
+            char* const pc = track_allocator::malloc(4 * partition_sz);
+            test_simp_seg_store tstore;
+            tstore.add_ordered_block(pc, 4 * partition_sz, partition_sz);
+
+            void* pvret = tstore.malloc_n(1, 2 * partition_sz);
+            BOOST_TEST(pvret == 0);
+
+            // There should still be two contiguous
+            //  and one non-contiguous chunk left
+            std::size_t nchunks = 0;
+            while(!tstore.empty())
+            {
+                tstore.malloc();
+                ++nchunks;
+            }
+            BOOST_TEST(nchunks == 4);
+        }
+
+        {
+            char* const pc = track_allocator::malloc(4 * partition_sz);
+            test_simp_seg_store tstore;
+            tstore.add_ordered_block(pc, 4 * partition_sz, partition_sz);
+
+            void* pvret = tstore.malloc_n(2, 2 * partition_sz);
+            BOOST_TEST(pvret == 0);
+
+            // There should still be two contiguous
+            //  and one non-contiguous chunk left
+            std::size_t nchunks = 0;
+            while(!tstore.empty())
+            {
+                tstore.malloc();
+                ++nchunks;
+            }
+            BOOST_TEST(nchunks == 4);
+        }
+
+        {
             char* const pc = track_allocator::malloc(12 * partition_sz);
             test_simp_seg_store tstore;
             tstore.add_ordered_block(pc, 2 * partition_sz, partition_sz);

commit 2822004af908c914762bc2b33e7aa39fbd6a9532

Demonstrate the regressions caused by (#53) (commit 8ec1be1) by unit tests

test/test_pool_alloc.cpp
@@ -294,6 +294,61 @@ void test_mem_usage()
     BOOST_TEST(track_alloc::ok());
 }
 
+void test_free_chunk_selection()
+{
+    typedef boost::pool<track_alloc> pool_type;
+
+    {
+        // Expose a regression from the commit 8ec1be1e82ba559744ecfa3c6ec13f71f9c175cc.
+        // Two checks will fail here.
+        pool_type pool(sizeof(void *), 3);
+        void * ptr_0 = pool.ordered_malloc(1);
+        void * ptr_1 = pool.ordered_malloc(1);
+        void * ptr_2 = pool.ordered_malloc(1);
+        // The blocks are expected to be allocated at subsequent locations
+        BOOST_TEST((char *)ptr_1 - (char *)ptr_0 == sizeof(void *));
+        BOOST_TEST((char *)ptr_2 - (char *)ptr_1 == sizeof(void *));
+
+        pool.ordered_free(ptr_1, 1);
+
+        void * ptr_1a = pool.ordered_malloc(1);
+        // Expected to reallocate the former ptr1 block
+        // which should be the first and only available block
+        BOOST_TEST(ptr_1a == ptr_1);
+
+        pool.ordered_free(ptr_0, 1);
+        pool.ordered_free(ptr_1a, 1);
+        pool.ordered_free(ptr_2, 1);
+    }
+
+    {
+        // Another way to expose a regression from the commit 8ec1be1e82ba559744ecfa3c6ec13f71f9c175cc.
+        // This time we preallocate 4 rather than 3 blocks in the pool. In this case
+        // the location of the ptr_2 block is as expected. The reallocation of ptr_1
+        // block however still fails the location expectation.
+        pool_type pool(sizeof(void *), 4);
+        void * ptr_0 = pool.ordered_malloc(1);
+        void * ptr_1 = pool.ordered_malloc(1);
+        void * ptr_2 = pool.ordered_malloc(1);
+        // The blocks are expected to be allocated at subsequent locations
+        BOOST_TEST((char *)ptr_1 - (char *)ptr_0 == sizeof(void *));
+        BOOST_TEST((char *)ptr_2 - (char *)ptr_1 == sizeof(void *));
+
+        pool.ordered_free(ptr_1, 1);
+
+        void * ptr_1a = pool.ordered_malloc(1);
+        // Expected to reallocate the former ptr1 block
+        // which should be the first available block
+        BOOST_TEST(ptr_1a == ptr_1);
+
+        pool.ordered_free(ptr_0, 1);
+        pool.ordered_free(ptr_1a, 1);
+        pool.ordered_free(ptr_2, 1);
+    }
+
+    BOOST_TEST(track_alloc::ok());
+}
+
 void test_void()
 {
     typedef boost::pool_allocator<void> void_allocator;
@@ -313,6 +368,7 @@ int main()
     test();
     test_alloc();
     test_mem_usage();
+    test_free_chunk_selection();
     test_void();
 
     return boost::report_errors();